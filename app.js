aa4377355a2e6aa8d6cc547e203219281461f307
WafaAlsabri <wafaalsabri23@gmail.com>
2022-02-22 07:34:39 +0300

    edit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import fetch from 'node-fetch';
 import path from 'path';
+import axios from 'axios';
 
 const app = express()
 
@@ -28,7 +29,7 @@ app.get('/product-detail_01', (req, res) => {
     res.render('product-detail_01')
 })
 
-app.get("/products?:p_id", (req, response) => {
+/*app.get("/products?:p_id", (req, response) => {
     if (!req.query.p_id) {
         fetch('https://dummyjson.com/products?select=title,price,rating,discountPercentage,thumbnail')
             .then(res => res.json())
@@ -44,30 +45,124 @@ app.get("/products?:p_id", (req, response) => {
             })
 
     }
-})
+})*/
+
+app.get('/products',async (req, res, next) => {
+    try {
+      let response = await axios.get('https://dummyjson.com/products');
+      res.render('products', {
+        products: response.data.products,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+});
+
+app.get('/products/categories',async(req, res, next) => {
+  try {
+    
+    let response = await axios.get(
+      `https://dummyjson.com/products/categories`
+    );
+    res.render('categories', {
+      products: response.data,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+
+app.get('/products/category/:category',async(req, res, next) => {
+  try {
+    const category = req.params.category;
+    let response = await axios.get(
+      `https://dummyjson.com/products/category/${category}`
+    );
+    res.render('category', {
+      products: response.data,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+
+app.get('/products/search', async (req, res, next) => {
+    try {
+      const query = req.query.q;
+      let response = await axios.get(
+        `https://dummyjson.com/products/search?q=${query}`
+      );
+  
+      res.render('search', {
+        products: response.data.products,
+      });
+    } catch (err) {
+      console.log(err);
+      res.send('<h1>Sorry There are no products to display.....</h1>')
+    }
+});
+  
+
+app.get('/products/:id', async (req, res, next) => {
+  try {
+    const productId = req.params.id;
+    let response = await axios.get(
+      `https://dummyjson.com/products/${productId}`
+    );
+
+    res.render('product', {
+      product: response.data,
+    });
+  } catch (err) {
+    console.log(err);
+    res.send('<h1>Sorry There are no products to display.....</h1>')
+  }
+});
 
 
 
 app.get('/shopping-cart', (req, res) => {
     res.render('shopping-cart')
 })
+
+app.get('/categories', (req, res) => {
+  res.render('categories')
+})
+app.get('/category', (req, res) => {
+  res.render('category')
+})
 app.get('/product-detail_01', (req, res) => {
     res.render('product-detail_01')
 })
-
+app.get('/product', (req, res) => {
+  res.render('product')
+})
 
 app.get('/index-header-2', (req, res) => {
     res.render('index-header-2')
 })
 
+app.get('/register', (req, res) => {
+  res.render('register')
+})
 app.get('/checkout', (req, res) => {
     res.render('checkout')
+})
+
+app.get('/contact', (req, res) => {
+  res.render('contact')
 })
 app.get('/blog-detail_01', (req, res) => {
     res.render('blog-detail_01')
 })
 app.get('/register', (req, res) => {
     res.render('register')
+})
+
+app.get('/checkout', (req, res) => {
+  res.render('checkout')
 })
 app.get('/people/:id', (req, res) => {
     // Database operation to get data...
